6ee28ac1da95f0c4520867eb7ef042b627220d7c
pietroscandola <pietro.scandolavr@gmail.com>
2022-02-02 14:46:49 +0100

    add searchUser

--- js/script.js
+++ js/script.js
@@ -13,6 +13,12 @@ Milestone 3
 
     - Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
     - Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+Milestone 5 - opzionale
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato (vedi immagine in allegato)
+Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti 
 */
 
 console.log('Vue ok', Vue);
@@ -142,15 +148,15 @@ const root = new Vue({
             this.contacts[this.currentIndex].messages.push(newObject);
         },
 
-        /* searchUser(index) {
-            const newSearch = this.newSearchUser.trim();
-            const result = contacts.filter((contact, index) => {
-                if (contact[index].name === newSearch) {
-                    return true;
+        searchUser() {
+            const newSearch = this.newSearchUser.trim().toLowerCase();
+            const result = this.contacts.filter((contact) => {
+                if (contact.name.toLowerCase().includes(newSearch)) {
+                    return contact.visible = true;
                 }
-                return false;
+                return contact.visible = false;
             })
-            console.log(result)
-        }, */
+            return result;
+        },
     },
 });
